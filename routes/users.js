5d32be0a0d59c10ac7833facff92d94023e6ed4d
klahori <klaho028@uottawa.ca>
2020-08-24 14:31:41 -0400

    can now delete users

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,8 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
-];
+let users = [];
 
 router.get("/", (req, res) => {
   console.log(users);
@@ -20,8 +10,17 @@ router.get("/", (req, res) => {
 });
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+  users.push({ ...user, id: uuidv4() });
   res.send(`User with the name${user.firstName} was added`);
 });
-
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`user with  id ${id} deleted`);
+});
 export default router;
